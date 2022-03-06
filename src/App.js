4d07af34b1843b2fb8bc401a7591c0958410187d
Sky <sky@sentbe.com>
2022-03-06 22:36:56 +0900

    feat: Define router and handler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import ProductListPage from './ProductListPage.js'
+import ProductDetailPage from './ProductDetailPage.js'
+import CartPage from './CartPage.js'
 
 export default function App ({ $target }) {
   this.route = () => {
     const { pathname } = location
     $target.innerHTML = ''
-    pathname === '/' && new ProductListPage({ $target }).render()
+
+    const router = {
+      '/products' () {
+        new ProductDetailPage({ $target }).render()
+      },
+      '/cart' () {
+        new CartPage({ $target }).render()
+      },
+    }
+
+    if (pathname === '/') return new ProductListPage({ $target }).render()
+
+    const [, path] = pathname.split('/')
+
+    router[`/${path}`] && router[`/${path}`]()
   }
 
   this.route()
